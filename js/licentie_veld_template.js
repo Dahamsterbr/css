ed910b3400f5b5e39c34eb2efc803934672ef7e2
Dahamster <6582577+blastboy@users.noreply.github.com>
2024-10-16 00:34:41 +0200

    Update licentie_veld_template.js

--- js/licentie_veld_template.js
+++ js/licentie_veld_template.js
@@ -11,34 +11,37 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateFields() {
         const veldType = veldTypeField.value;
 
-        // Toon of verberg de veld containers
+        // Toon of verberg de veldcontainers en verwijder de hidden class
         if (veldType === "boolean") {
             booleanFieldContainer.style.display = "block";
+            booleanFieldContainer.classList.remove("hidden");
             integerFieldContainer.style.display = "none";
             textFieldContainer.style.display = "none";
 
             // Zorg ervoor dat de juiste veldtypen zichtbaar zijn
-            booleanField.type = "checkbox";  // Dit kan afhankelijk zijn van je gebruik (checkbox voor boolean)
+            booleanField.type = "checkbox";
             integerField.type = "hidden";
             textField.type = "hidden";
         } else if (veldType === "integer") {
             booleanFieldContainer.style.display = "none";
             integerFieldContainer.style.display = "block";
+            integerFieldContainer.classList.remove("hidden");
             textFieldContainer.style.display = "none";
 
             // Zorg ervoor dat de juiste veldtypen zichtbaar zijn
             booleanField.type = "hidden";
-            integerField.type = "number";  // Gebruik 'number' voor integer
+            integerField.type = "number";
             textField.type = "hidden";
         } else if (veldType === "text") {
             booleanFieldContainer.style.display = "none";
             integerFieldContainer.style.display = "none";
             textFieldContainer.style.display = "block";
+            textFieldContainer.classList.remove("hidden");
 
             // Zorg ervoor dat de juiste veldtypen zichtbaar zijn
             booleanField.type = "hidden";
             integerField.type = "hidden";
-            textField.type = "text";  // Gebruik 'text' voor tekstvelden
+            textField.type = "text";
         } else {
             // Verberg alle velden als er geen veld_type is gekozen
             booleanFieldContainer.style.display = "none";
